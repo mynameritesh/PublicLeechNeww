5d0aa0344b521ca2e52a60ec5067af85af6765e7
viraj shah <virajshah9896@gmail.com>
2020-06-29 10:39:57 +0530

    Update new_join_fn.py

--- tobrot/plugins/new_join_fn.py
+++ tobrot/plugins/new_join_fn.py
@@ -38,7 +38,7 @@ async def help_message_f(client, message):
     message_id = 99
     # display the /help message
     await message.reply_text(
-        f"Most Welcome please read the Pinned Message",
+        f"Welcome To Cloud Downloader Please read the Pinned Message",
         quote=True
     )
 
